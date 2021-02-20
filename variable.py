3034e6fbcdad8cbb0a5a16a68c1e19197f7bdd1d
Maco <miw_hadis@msn.com>
2021-02-20 20:11:29 +0800

    变量使用

---
+++ variable.py
@@ -0,0 +1,22 @@
+age = 0
+age = 18
+
+# 查看变量的类型
+print("变量类型为:", type(age))
+# 查看变量的地址
+print("变量地址为:", id(age))
+# 输出变量的值
+print("age:", age)
+
+#数字计算
+num3 = 12
+num4 = 13
+print(num3 + num4)
+
+'''
+# 字符串转数字计算
+num1 = int(input("数字1: "))
+num2 = int(input("数字2: "))
+
+print("num1 + num2" =, num1 + num2)
+'''
